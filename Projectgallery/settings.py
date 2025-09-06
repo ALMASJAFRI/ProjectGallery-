5358af334694960d80f9abcc7cde0063ad3f5917
ALMASJAFRI <almasjafri12@gmail.com>
2025-09-06 22:33:55 +0530

    Update settings.py

--- Projectgallery/settings.py
+++ Projectgallery/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,7 +146,8 @@ EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
 
 STATIC_URL="static/"
 STATICFILES_DIRS=[BASE_DIR/'static']
-STATIC_ROOT=BASE_DIR/"statics"
+STATIC_ROOT=BASE_DIR/"staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LLM_KEY=config('LLM_KEY')
 
