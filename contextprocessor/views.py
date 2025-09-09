7e304a6d90d4f253ff647dd6dee24c25390cb2f9
ALMASJAFRI <almasjafri12@gmail.com>
2025-09-09 12:44:33 +0530

    Update views.py

--- contextprocessor/views.py
+++ contextprocessor/views.py
@@ -7,9 +7,9 @@ from django.http import HttpResponse
 def create_superuser(request):
     User = get_user_model()
     # ✅ Change username, email, and password as you like
-    username = 'admin'
-    email = 'admin@example.com'
-    password = 'StrongPassword123'
+    username = 'Almas'
+    email = 'djangoprojects954@gmail.com'
+    password = '12345'
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
