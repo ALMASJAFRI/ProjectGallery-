5ee24b5da1029fa657bda3a879467f99c189d215
ALMASJAFRI <almasjafri12@gmail.com>
2025-09-09 12:43:14 +0530

    Update views.py

--- contextprocessor/views.py
+++ contextprocessor/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+def create_superuser(request):
+    User = get_user_model()
+    # ✅ Change username, email, and password as you like
+    username = 'admin'
+    email = 'admin@example.com'
+    password = 'StrongPassword123'
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        return HttpResponse("Superuser created successfully!")
+    return HttpResponse("Superuser already exists.")
