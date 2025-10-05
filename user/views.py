80d88665d70ba303562ebd215c293bf4c8f93e16
ALMASJAFRI <almasjafri12@gmail.com>
2025-10-05 09:07:14 +0530

    Update views.py

--- user/views.py
+++ user/views.py
@@ -161,9 +161,9 @@ def validatepay(request):
             order.status=True
             order.save()
             projectins=order.det
-            #t=threading.Thread(target=sendfiles,args=(amount,projectins.id,email))
-            #t.start()
-            sendfiles(amount,projectins.id,email)
+            t=threading.Thread(target=sendfiles,args=(amount,projectins.id,email))
+            t.start()
+            #sendfiles(amount,projectins.id,email)
             messages.success(request,"Payment Done")
             data={'txnid':txnid,'email':email,'product':productinfo,'amount':amount,'paymentid':mihpayid}
             return render(request,"accounts/successpage.html",data)
