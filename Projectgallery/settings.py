10a85bacd6ee48980aa0d3d71ff20c870bb2202c
ALMASJAFRI <almasjafri12@gmail.com>
2025-10-06 18:17:13 +0530

    Update settings.py

--- Projectgallery/settings.py
+++ Projectgallery/settings.py
@@ -155,12 +155,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL='accounts.Custom_User'
 
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT =587 #config('EMAIL_PORT',cast=int)
-EMAIL_USE_TLS =config('EMAIL_USE_TLS',cast=bool)
-EMAIL_HOST_USER =config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
+#EMAIL_BACKEND = config('EMAIL_BACKEND')
+#EMAIL_HOST = config('EMAIL_HOST')
+#EMAIL_PORT =587 #config('EMAIL_PORT',cast=int)
+#EMAIL_USE_TLS =config('EMAIL_USE_TLS',cast=bool)
+#EMAIL_HOST_USER =config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 STATIC_URL="static/"
 STATICFILES_DIRS=[BASE_DIR/'static']
